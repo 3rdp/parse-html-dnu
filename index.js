dfbcdbefd3c725adb966cc03f7f5aa68b89bbf26
3rdp <third.pancake16@gmail.com>
2018-06-18 20:50:59 +0300

    GitHub refactoring; hope it still works now

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const cheerio = require("cheerio")
 const fs =      require("fs")
 
-let html = fs.readFileSync("source.html", "utf8")
-let $ = cheerio.load(html)
-let table = $("#ContentPlaceHolder1_GridView1")
-let tableColumnNames = `
+const html = fs.readFileSync("source.html", "utf8")
+const $ = cheerio.load(html)
+const table = $("#ContentPlaceHolder1_GridView1")
+const tableColumnNames = `
 num
 0
 name
@@ -18,13 +18,13 @@ orig
 table.find("tr:first-child").remove()
 
 // loop through rows
-let students = table.find("tr").map(function() {
-  let columns = $(this).find("td")
-  let student = {}
+const students = table.find("tr").map(function() {
+  const columns = $(this).find("td")
+  const student = {}
 
   columns.each(function(index) {
     if (index == 1) return
-    let fieldName = tableColumnNames[index]
+    const fieldName = tableColumnNames[index]
 
     student[fieldName] = $(this).text().trim()
   })
@@ -33,18 +33,19 @@ let students = table.find("tr").map(function() {
 
 // console.log(students)
 // make our work easier
+const COMMA_TO_DOT = /,/,"."
 
 // now let's sort some data
-var studentsBy = students.sort(function (a, b) {
-  let balA_ekz = Number(a.bal_ekz.replace(/,/,"."))
-  let balA_diploma = Number(a.bal_diploma.replace(/,/,"."))
-  let balA = balA_ekz + balA_diploma
+const studentsBy = students.sort(function (a, b) {
+  const balA_ekz = Number(a.bal_ekz.replace(COMMA_TO_DOT))
+  const balA_diploma = Number(a.bal_diploma.replace(COMMA_TO_DOT))
+  const balA = balA_ekz + balA_diploma
 
-  let balB_ekz = Number(b.bal_ekz.replace(/,/,"."))
-  let balB_diploma = Number(b.bal_diploma.replace(/,/,"."))
-  let balB = balB_ekz + balB_diploma
+  const balB_ekz = Number(b.bal_ekz.replace(COMMA_TO_DOT))
+  const balB_diploma = Number(b.bal_diploma.replace(COMMA_TO_DOT))
+  const balB = balB_ekz + balB_diploma
 
-  return balB-balA
+  return balB - balA
 })
 
 console.log(studentsBy)
